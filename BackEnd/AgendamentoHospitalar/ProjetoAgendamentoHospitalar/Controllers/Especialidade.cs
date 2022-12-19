b080833c06f90def9b37ea01d580db78894cc312
Daphyne Pires <daphyne.pires@gmail.com>
2022-12-19 14:24:48 -0300

    Estruturando Projeto

---
+++ BackEnd/AgendamentoHospitalar/ProjetoAgendamentoHospitalar/Controllers/Especialidade.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoAgendamentoHospitalar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Especialidade : ControllerBase
+    {
+    }
+}
