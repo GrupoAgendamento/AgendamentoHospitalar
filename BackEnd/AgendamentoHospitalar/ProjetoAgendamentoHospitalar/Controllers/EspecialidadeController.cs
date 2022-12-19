f61aab04641d674a929c61d5f4f46648f70dce4c
Daphyne Pires <daphyne.pires@gmail.com>
2022-12-19 15:32:19 -0300

    Adicionando a EspecialidadeController

---
+++ BackEnd/AgendamentoHospitalar/ProjetoAgendamentoHospitalar/Controllers/EspecialidadeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoAgendamentoHospitalar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EspecialidadeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/ConsultarEspecialidades")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Models.Especialidade))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public Models.Especialidade ConsultarEspecialidades()
+        {
+            return new Models.Especialidade();
+        }
+
+        [HttpGet]
+        [Route("/ConsultarPorId/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Models.Especialidade))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public Models.Especialidade ListarPorId(int id) 
+        {
+            return new Models.Especialidade();
+        }
+
+        [HttpPost]
+        [Route("/CadastrarEspecialidade")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public int CadastrarEspecialidade(Models.Especialidade)
+        {
+            return 0;
+        }
+
+        [HttpDelete]
+        [Route("/Deletar")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public int DeletarEspecialidade(Models.Especialidade)
+        {
+            return 0;
+        }
+        [HttpPatch]
+        [Route("/Atualizar")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public int AtualizarEspecialiade(Models.Especialidade)
+        {
+            return 0;
+        }
+    }
+}
