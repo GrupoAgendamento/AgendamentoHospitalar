3cce5c695a2298fd10f8cd960de9cb966d5109d8
Alexsander Silva <alexsander.silva14@live.com>
2022-12-21 12:54:38 -0300

    Deploy

--- FrontEnd/AgendamentoMedico/src/app/app-routing.module.ts
+++ FrontEnd/AgendamentoMedico/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HospitalEditarComponent } from './hospital/hospital-editar/hospital-edi
 import { HospitalListaComponent } from './hospital/hospital-lista/hospital-lista.component';
 import { ProfissionalEditarComponent } from './Profissional/Profissional-editar/profissional-editar.component';
 import { ProfissionalListaComponent } from './Profissional/Profissional-lista/profissional-lista.component';
-import { AgendamentoCadastrarComponent } from './Agendamento/agendar/agendar.component';
+import { AgendarComponent } from './Agendamento/agendar/agendar.component';
 import { ConsultarComponent } from './Agendamento/consultar/consultar.component';
 
 const routes: Routes = [
@@ -19,7 +19,7 @@ const routes: Routes = [
   { path: 'hospitallista', component: HospitalListaComponent },
   { path: 'profissionalcadastrar', component: ProfissionalEditarComponent },
   { path: 'profissionallista', component: ProfissionalListaComponent },
-  { path: '', component: AgendamentoCadastrarComponent },
+  { path: '', component: AgendarComponent },
   { path: 'agendamentoconsultar', component: ConsultarComponent },
   { path: 'profissionalcadastrar/:id', component: ProfissionalEditarComponent },
   { path: 'profissionallista', component: ProfissionalListaComponent },
