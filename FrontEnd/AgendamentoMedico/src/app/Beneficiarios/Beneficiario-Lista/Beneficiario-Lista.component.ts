4f2cd450959b25c2651a3a93aab769cba7f09d46
Isabella Sanchis <isasanchis3@gmail.com>
2022-12-21 11:28:03 -0300

    Alterando componentes Beneficiario e Hospital

--- FrontEnd/AgendamentoMedico/src/app/Beneficiarios/Beneficiario-Lista/Beneficiario-Lista.component.ts
+++ FrontEnd/AgendamentoMedico/src/app/Beneficiarios/Beneficiario-Lista/Beneficiario-Lista.component.ts
@@ -1,28 +1,50 @@
+import { Component } from "@angular/core";
+import { IBeneficiarioDto } from "src/app/interfaces/IBeneficiarioDto";
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
-  selector: 'app-Beneficiario-Lista',
-  templateUrl: './Beneficiario-Lista.component.html',
-  styleUrls: ['./Beneficiario-Lista.component.css']
+    selector: 'app-beneficiario-lista',
+    templateUrl: './Beneficiario-lista.component.html',
+    styleUrls: ['./Beneficiario-lista.component.css']
 })
-export class BeneficiarioListaComponent implements OnInit {
 
-  public beneficiarios: any = [];
-  frase: string = "";
+export class BeneficiarioListaComponent {
+  beneficiarioLista: IBeneficiarioDto[] = [];
 
-  constructor(private http: HttpClient) { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient, private router: Router) {
     this.getBeneficiarios();
   }
 
-  public getBeneficiarios(){
-    this.http.get('https://localhost:7026/api/Beneficiario').subscribe(
-      response => {this.beneficiarios = response; },
-      error => console.log(error)
-    );
+  getBeneficiarios() {
+    this.beneficiarioLista = [];
+    this.http.get('https://localhost:7026/api/Beneficiario')
+    .pipe(
+      map((response: any) => {
+        return Object.values(response);
+      })
+    )
+    .subscribe((data) => {
+      for(let index = 0; index < data.length; index++) {
+        let json: any = data[index];
+        this.beneficiarioLista.push(json as IBeneficiarioDto);
+      }
+    });
   }
 
+  removerBeneficiario(id: number) {
+    this.http.delete(`https://localhost:7026/api/Beneficiario/${id}`)
+    .subscribe(() => {
+      this.getBeneficiarios();
+    });
+  }
+  
+  editarBeneficiario(id: number) {
+    this.router.navigate([`beneficiariocadastrar/${id}`]);
+  }
 
+  adicionarBeneficiario(){
+    this.router.navigate([`beneficiariocadastrar`]);
+  }
 }
--- FrontEnd/AgendamentoMedico/src/app/app-routing.module.ts
+++ FrontEnd/AgendamentoMedico/src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router';
-import { BeneficiarioCadastrarComponent} from './Beneficiarios/Beneficiario-Cadastrar/Beneficiario-Cadastrar.component'
-import { BeneficiarioListaComponent} from './Beneficiarios/Beneficiario-Lista/Beneficiario-Lista.component'
+import { BeneficiarioEditarComponent} from './Beneficiarios/Beneficiario-editar/Beneficiario-editar.component'
+import { BeneficiarioListaComponent} from './Beneficiarios/Beneficiario-lista/Beneficiario-lista.component'
 import { HospitalEditarComponent } from './hospital/hospital-editar/hospital-editar.component';
 import { HospitalListaComponent } from './hospital/hospital-lista/hospital-lista.component';
 import { ProfissionalEditarComponent } from './Profissional/Profissional-editar/profissional-editar.component';
 import { ProfissionalListaComponent } from './Profissional/Profissional-lista/profissional-lista.component';
 
 const routes: Routes = [
-  { path: 'beneficiariocadastrar', component: BeneficiarioCadastrarComponent },
+  { path: 'beneficiariocadastrar', component: BeneficiarioEditarComponent },
+  { path: 'beneficiariocadastrar/:id', component: BeneficiarioEditarComponent },
   { path: 'beneficiariolista', component: BeneficiarioListaComponent },
   { path: 'hospitalcadastrar', component: HospitalEditarComponent },
-  { path: 'hospitallista', component: HospitalListaComponent },
   { path: 'hospitalcadastrar/:id', component: HospitalEditarComponent },
+  { path: 'hospitallista', component: HospitalListaComponent },
   { path: 'profissionalcadastrar', component: ProfissionalEditarComponent },
-  { path: 'profissionallista', component: ProfissionalListaComponent },
   { path: 'profissionalcadastrar/:id', component: ProfissionalEditarComponent },
+  { path: 'profissionallista', component: ProfissionalListaComponent },
 ];
 
 @NgModule({
