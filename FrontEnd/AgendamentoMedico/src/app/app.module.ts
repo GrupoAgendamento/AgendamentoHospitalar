f42d580d039fb2a81be72cbe3bcad983cfe56011
Laura Montenegro <larislakis@hotmail.com>
2022-12-22 11:06:13 -0300

    att front editar

--- FrontEnd/AgendamentoMedico/src/app/app.module.ts
+++ FrontEnd/AgendamentoMedico/src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { BeneficiarioEditarComponent } from './Beneficiarios/Beneficiario-editar/Beneficiario-editar.component';
+import { ProfissionalEditarComponent } from './Profissional/Profissional-editar/profissional-editar.component';
+import { ProfissionalListaComponent } from './Profissional/Profissional-lista/profissional-lista.component';
+import { ProfissionalCadastrarComponent } from './Profissional/Profissional-cadastrar/profissional-cadastrar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -5,31 +9,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BeneficiarioEditarComponent } from './Beneficiarios/Beneficiario-Editar/Beneficiario-editar.component';
+
 import { AppComponent } from './app.component';
+import { BeneficiarioCadastrarComponent } from './Beneficiarios/Beneficiario-Cadastrar/Beneficiario-Cadastrar.component';
 import { BeneficiarioListaComponent } from './Beneficiarios/Beneficiario-Lista/Beneficiario-Lista.component';
 import { HospitalListaComponent } from './hospital/hospital-lista/hospital-lista.component';
 import { HospitalEditarComponent } from './hospital/hospital-editar/hospital-editar.component';
-import { ProfissionalEditarComponent } from './Profissional/Profissional-editar/profissional-editar.component';
-import { ProfissionalListaComponent } from './Profissional/Profissional-lista/profissional-lista.component';
 import { NavComponent } from './Nav/Nav.component';
-import { BeneficiarioCadastrarComponent } from './Beneficiarios/Beneficiario-Cadastrar/Beneficiario-Cadastrar.component';
-import { AgendarComponent } from './Agendamento/agendar/agendar.component';
-import { ConsultarComponent } from './Agendamento/consultar/consultar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BeneficiarioCadastrarComponent,
     BeneficiarioListaComponent,
+    BeneficiarioCadastrarComponent,
     BeneficiarioEditarComponent,
-    ProfissionalEditarComponent,
-    ProfissionalListaComponent,
     HospitalListaComponent,
     HospitalEditarComponent,
-    NavComponent,
-    AgendarComponent,
-    ConsultarComponent,
+    ProfissionalCadastrarComponent,
+    ProfissionalListaComponent,
+    ProfissionalEditarComponent,
+    NavComponent
   ],
   imports: [
     AppRoutingModule,
