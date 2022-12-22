0b8bff660d2142b754dfba7b6688214e8b75642d
Laura Montenegro <larislakis@hotmail.com>
2022-12-22 11:53:56 -0300

    att

--- FrontEnd/AgendamentoMedico/src/app/interfaces/IBeneficiarioDto.ts
+++ FrontEnd/AgendamentoMedico/src/app/interfaces/IBeneficiarioDto.ts
@@ -1,10 +1,11 @@
-export interface IBeneficiarioDto {
-    nome: string,
-    cpf: string,
-    telefone?: string,
-    email: string,
-    endereco?: string,
-    numeroCarteirinha: string,
-    senha: string,
-    ativo: boolean
+export interface  IBeneficiarioDTO
+{
+  nome: string;
+  cpf: string;
+  telefone: string;
+  email: string;
+  endereco: string;
+  numeroCarteirinha: string;
+  senha: string;
+  ativo: boolean;
 }
